3146e90c33ea957b90963033272df1d18d2d92ed
Arun Allamsetty <arun@instructure.com>
2016-12-30 11:36:09 -0700

    Finished exericises in Chapter 2.

--- src/main/scala/co/aa8y/book/fpinscala/Chapter2.scala
+++ src/main/scala/co/aa8y/book/fpinscala/Chapter2.scala
@@ -9,4 +9,31 @@ object Chapter2 {
     if (n <= 1) n
     else fib(n - 2) + fib(n - 1)
   }
+
+  // Exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(n: Int, nPlus1: Int): Boolean = {
+      if (nPlus1 > as.size - 1) true
+      else ordered(as(n), as(nPlus1)) && loop(n + 1, nPlus1 + 1)
+    }
+    if (as.size <= 1) true
+    else loop(0, 1)
+  }
+
+  // Exercise 2.3
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => {
+      (b: B) => f(a, b)
+    }
+  }
+
+  // Exercise 2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  // Exercise 2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
 }
--- src/test/scala/co/aa8y/book/fpinscala/Chapter2Spec.scala
+++ src/test/scala/co/aa8y/book/fpinscala/Chapter2Spec.scala
@@ -20,4 +20,36 @@ class Chapter2Spec extends FlatSpec {
     assert(fib(5) === 5)
     assert(fib(6) === 8)
   }
+
+  "isSorted()" should "check if an array is sorted based on the comparison function." in {
+    assert(isSorted(Array(1, 4, 5), (x: Int, y: Int) => x <= y) === true)
+    assert(isSorted(Array(4, 5, 3), (x: Int, y: Int) => x <= y) === false)
+    assert(isSorted(Array(5, 3, 1), (x: Int, y: Int) => x >= y) === true)
+    assert(isSorted(Array("foo", "bar", "baz"), (x: String, y: String) => x <= y) === false)
+    assert(isSorted(Array("bar", "baz", "foo"), (x: String, y: String) => x <= y) === true)
+  }
+
+  it should "always return true for an empty array or an array." in {
+    assert(isSorted(Array.empty, (x: Int, y: Int) => x <= y) === true)
+    assert(isSorted(Array(1), (x: Int, y: Int) => false) === true)
+  }
+
+  "curry()" should "curry." in {
+    val input = (c: Char, i: Int) => (c + i).toChar.toString
+    val output = curry(input)
+    assert(input('a', 1) === output('a')(1))
+  }
+
+  "uncurry()" should "undo a curry." in {
+    val input = (c: Char) => { (i: Int) => (c + i).toChar.toString }
+    val output = uncurry(input)
+    assert(input('a')(1) === output('a', 1))
+  }
+
+  "compose()" should "compose two functions by combining them." in {
+    val incrementChar = (c: Char) => (c + 1)
+    val intAsCharString = (i: Int) => i.toChar.toString
+    val nextCharAsString = compose(intAsCharString, incrementChar)
+    assert(nextCharAsString('a') === "b")
+  }
 }
