f823dfe3e0ea509142877a19cc064487e43acef0
Arun Allamsetty <arun.allamsetty@gmail.com>
2017-05-02 22:30:21 -0600

    Fixing Chapter 2 solutions.

--- src/main/scala/co/aa8y/book/fpinscala/Chapter2.scala
+++ src/main/scala/co/aa8y/book/fpinscala/Chapter2.scala
@@ -4,17 +4,29 @@ import scala.annotation.tailrec
 
 object Chapter2 {
   // Exercise 2.1
-  def fib(n: Int): Int = {
+  def naivefib(n: Int): Int = {
     if (n < 0) throw new IllegalArgumentException(s"n cannot be less than 0. $n was passed.")
     if (n <= 1) n
     else fib(n - 2) + fib(n - 1)
   }
 
+  def fib(n: Int): Int = {
+    if (n < 0) throw new IllegalArgumentException(s"n cannot be less than 0. $n was passed.")
+    @tailrec
+    def loop(count: Int, p: Int, q: Int): Int = {
+      if (count >= n) p
+      else loop(count + 1, q, p + q)
+    }
+    loop(0, 0, 1)
+  }
+
   // Exercise 2.2
   def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-    def loop(n: Int, nPlus1: Int): Boolean = {
-      if (nPlus1 > as.size - 1) true
-      else ordered(as(n), as(nPlus1)) && loop(n + 1, nPlus1 + 1)
+    @tailrec
+    def loop(m: Int, n: Int): Boolean = {
+      if (n == as.size) true
+      else if (!ordered(as(m), as(n))) false
+      else loop(n, n + 1)
     }
     if (as.size <= 1) true
     else loop(0, 1)
